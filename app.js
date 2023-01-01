d9de734c3fb7319e8a163aabdf28e3091d0e3c49
kadev <karimaoudiaa@gmail.com>
2023-01-01 17:37:01 +0100

    ajout exo 4 bis validator /config joy

--- app.js
+++ app.js
@@ -9,22 +9,31 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-const { hashPassword } = require("./auth.js");
+
+
+//const { hashPassword } = require("./auth.js");
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
+const { validateMovie,validateUser } = require("./validators.js");
+
 
 
 app.use(express.json());
 app.get("/", welcome);
 
 
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 
 app.get("/api/users", userHandlers.getUsers); 
 app.get("/api/users/:id", userHandlers.getUserById); 
-app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post("/api/users", validateUser,userHandlers.postUser);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
